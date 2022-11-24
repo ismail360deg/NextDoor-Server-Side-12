7d94706db4146b328729d02199adc2c6b383eec6
ismail360deg <ismailbhuiyan2000@gmail.com>
2022-11-24 16:49:17 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+
+app.get('/', async (req, res) => {
+    res.send('nextDoor server is running');
+})
+
+app.listen(port, () => console.log(`nextDoor running on ${port}`))
