8bc2393f2cca59048a88525254243ef94e8bbcf0
ismail360deg <ismailbhuiyan2000@gmail.com>
2022-11-27 22:04:46 +0600

    payment emplement done

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 const port = process.env.PORT || 5000;
 
@@ -44,6 +45,7 @@ async function run() {
         const productCollection = client.db('nextDoor').collection('products');
         const usersCollection = client.db('nextDoor').collection('users');
         const myProductsCollection = client.db('nextDoor').collection('myProducts');
+        const paymentsCollection = client.db('nextDoor').collection('payments');
 
         const verifyAdmin = async (req, res, next) => {
             const decodedEmail = req.decoded.email;
@@ -55,6 +57,13 @@ async function run() {
             }
             next();
         }
+
+        app.get('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const booking = await productCollection.findOne(query);
+            res.send(booking);
+        })
 
         app.get('/category', async (req, res) => {
             if (req.query.brand) {
@@ -70,11 +79,6 @@ async function run() {
             }
         })
 
-        app.get('/appointmentSpecialty', async (req, res) => {
-            const query = {}
-            const result = await appointmentOptionCollection.find(query).project({ name: 1 }).toArray();
-            res.send(result);
-        })
 
         app.get('/cardInfo', async (req, res) => {
             const query = {};
@@ -107,6 +111,38 @@ async function run() {
             const result = await productCollection.insertOne(booking);
             res.send(result);
         });
+
+        app.post('/create-payment-intent', async (req, res) => {
+            const booking = req.body;
+            const price = booking.price;
+            const amount = price * 100;
+
+            const paymentIntent = await stripe.paymentIntents.create({
+                currency: 'usd',
+                amount: amount,
+                "payment_method_types": [
+                    "card"
+                ]
+            });
+            res.send({
+                clientSecret: paymentIntent.client_secret,
+            });
+        });
+
+        app.post('/payments', async (req, res) => {
+            const payment = req.body;
+            const result = await paymentsCollection.insertOne(payment);
+            const id = payment.bookingId
+            const filter = { _id: ObjectId(id) }
+            const updatedDoc = {
+                $set: {
+                    paid: true,
+                    transactionId: payment.transactionId
+                }
+            }
+            const updatedResult = await productCollection.updateOne(filter, updatedDoc)
+            res.send(result);
+        })
 
         app.get('/jwt', async (req, res) => {
             const email = req.query.email;
